e57d714fd7742089cad2a8c70cddfe6778852e68
Felix Albrecht <mail@felixalbrecht.de>
2013-01-09 22:54:20 +0100

    [common] added external_libraries_cfg_filename

--- common.py
+++ common.py
@@ -16,17 +16,19 @@ except OSError, os_error:
     if os_error.errno != 17:
         raise os_error
 
+external_libraries_cfg_filename = join(_BASEDIR, 'external-libraries.cfg')
+
 def BASEDIR():
     return _BASEDIR
 
 def SRCDIR():
     return _SRCDIR
 
-_CC=''
-_CXX=''
-_F77=''
-_CXXFLAGS=''
-_config_opts_parsed=False
+_CC = ''
+_CXX = ''
+_F77 = ''
+_CXXFLAGS = ''
+_config_opts_parsed = False
 
 def _parse_config_opts():
     # use global variables
@@ -37,9 +39,9 @@ def _parse_config_opts():
     global _config_opts_parsed
 
     # get CC from environment
-    env_CC=''
+    env_CC = ''
     try:
-        env_CC=os.environ['CC']
+        env_CC = os.environ['CC']
     except AttributeError:
         raise Exception('ERROR: missing environment variable \'CC\'!')
     except KeyError:
@@ -48,9 +50,9 @@ def _parse_config_opts():
         raise
 
     # read corresponding config.opts
-    config_opts_filename='config.opts.' + env_CC
-    filename=join(_BASEDIR, config_opts_filename)
-    print('reading from \'{filename}\':'.format(filename=config_opts_filename))
+    config_opts_filename = 'config.opts.' + env_CC
+    filename = join(_BASEDIR, config_opts_filename)
+#    print('reading from \'{filename}\':'.format(filename=config_opts_filename))
     try:
         config_opts = open(filename).read()
     except:
@@ -72,49 +74,38 @@ def _parse_config_opts():
         tmp = _CXXFLAGS
         _CXXFLAGS = _CXXFLAGS.split(' ')[0]
         config_opts += ' ' + tmp[tmp.index(_CXXFLAGS) + len(_CXXFLAGS):]
-    
+
     # then read CC, CXX and F77
     def find_that_is_not_one_of(string, rest):
         try:
-            exceptional_msg='ERROR: no suitable \'{string}=some_exe\' found in \'{filename}\'!'.format(string=string,
+            exceptional_msg = 'ERROR: no suitable \'{string}=some_exe\' found in \'{filename}\'!'.format(string=string,
                 filename=filename)
             #print('config_opts = \'{config_opts}\''.format(config_opts=config_opts))
-            after=config_opts[config_opts.index(string) + len(string):]
+            after = config_opts[config_opts.index(string) + len(string):]
             #print('after = \'{after}\''.format(after=after))
             if after[0] != '=' or after[1] == ' ':
                 raise Exception(exceptional_msg)
-            possible=after[1:].split('=')[0].split()[0]
+            possible = after[1:].split('=')[0].split()[0]
             if possible in rest:
                 raise Exception(exceptional_msg)
             else:
                 return possible
         except:
             raise
-    
-    _CC=find_that_is_not_one_of('CC', ['CXX', 'F77', 'CXXFLAGS'])
-    _CXX=find_that_is_not_one_of('CXX', ['CC', 'F77', 'CXXFLAGS'])
-    _F77=find_that_is_not_one_of('F77', ['CC', 'CXX', 'CXXFLAGS'])
-    
-    # print summary
-    print('  CC={CC}'.format(CC=_CC))
-    print('  CXX={CXX}'.format(CXX=_CXX))
-    print('  CXXFLAGS=\'{CXXFLAGS}\''.format(CXXFLAGS=_CXXFLAGS))
-    print('  F77={F77}'.format(F77=_F77))
-    
+
+    _CC = find_that_is_not_one_of('CC', ['CXX', 'F77', 'CXXFLAGS'])
+    _CXX = find_that_is_not_one_of('CXX', ['CC', 'F77', 'CXXFLAGS'])
+    _F77 = find_that_is_not_one_of('F77', ['CC', 'CXX', 'CXXFLAGS'])
+
+#    # print summary
+#    print('  CC={CC}'.format(CC=_CC))
+#    print('  CXX={CXX}'.format(CXX=_CXX))
+#    print('  CXXFLAGS=\'{CXXFLAGS}\''.format(CXXFLAGS=_CXXFLAGS))
+#    print('  F77={F77}'.format(F77=_F77))
+
     # finished
-    _config_opts_parsed=True
+    _config_opts_parsed = True
 
-
-# extract function
-def extract_in_srcdir(tarfile, untared_name, target_name):
-    if not exists(join(_SRCDIR, target_name)):
-        if not exists(join(_SRCDIR, untared_name)):
-            if exists(join(_SRCDIR, tarfile)):
-                subprocess.call(['tar', 'xzf', tarfile], cwd=_SRCDIR)
-            else:
-                raise Exception('Error: %s does not exist'
-                                % join(_SRCDIR, tarfile))
-            shutil.move(join(_SRCDIR, untared_name), join(_SRCDIR, target_name))
 
 def CC():
     if not _config_opts_parsed:
