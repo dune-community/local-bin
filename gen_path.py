f3b84d3dbaa45d9059800e7e11fb9cfdc64a759d
Felix Albrecht <felix.albrecht@uni-muenster.de>
2013-01-09 15:52:10 +0100

    [gen_path] creates PATH.sh

---
+++ gen_path.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+#import os
+from os.path import join
+#import subprocess
+#import sys
+
+import common as common
+
+with open(join(common.BASEDIR(), 'PATH.sh'), 'w') as pathfile:
+    pathfile.write('export BASEDIR={BASEDIR}\n'.format(BASEDIR=common.BASEDIR()))
+    pathfile.write('export PATH=$BASEDIR/local/bin:$PATH\n')
+    pathfile.write('export LD_LIBRARY_PATH=$BASEDIR/local/lib:$LD_LIBRARY_PATH\n')
+    pathfile.write('export PKG_CONFIG_PATH=$BASEDIR/local/lib/pkgconfig:$PKG_CONFIG_PATH\n')
+    pathfile.write('export CC={CC}\n'.format(CC=common.CC()))
+    pathfile.write('export CXX={CXX}\n'.format(CXX=common.CXX()))
+    pathfile.write('export F77={F77}\n'.format(F77=common.F77()))
+    pathfile.write('export CXXFLAGS=\'{CXXFLAGS}\'\n'.format(CXXFLAGS=common.CXXFLAGS()))
