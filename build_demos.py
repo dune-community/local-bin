f68566267949d7ddedd7a4588e1a158eb5c62906
Felix Albrecht <felix.albrecht@uni-muenster.de>
2013-01-10 17:14:17 +0100

    [build_demos] added

---
+++ build_demos.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import ConfigParser
+import os
+from os.path import join
+import subprocess
+import sys
+
+import common
+
+filename = common.demos_cfg_filename
+
+
+def build_demo(demo, build_commands):
+    demo_dir = join(common.BASEDIR(), 'demos')
+    try:
+        os.mkdir(demo_dir)
+    except OSError, os_error:
+        if os_error.errno != 17:
+            raise os_error
+    for build_command in build_commands.split(','):
+        build_command = build_command.lstrip().rstrip()
+        if not (build_command[0] == '\'' and build_command[-1] == '\''):
+            raise Exception('build commands have to be of the form \'command_1\' (is {cmd})!'.format(cmd=build_command))
+        build_command = build_command[1:-1].lstrip().rstrip()
+        build_command = build_command.replace('$BASEDIR', '{BASEDIR}'.format(BASEDIR=common.BASEDIR()))
+        build_command = build_command.replace('$SRCDIR', '{SRCDIR}'.format(SRCDIR=common.SRCDIR()))
+        build_command = build_command.replace('$CXXFLAGS', '\'{CXXFLAGS}\''.format(CXXFLAGS=common.CXXFLAGS()))
+        build_command = build_command.replace('$CC', '{CC}'.format(CC=common.CC()))
+        build_command = build_command.replace('$CXX', '{CXX}'.format(CXX=common.CXX()))
+        build_command = build_command.replace('$F77', '{F77}'.format(F77=common.F77()))
+        print('  calling \'{build_command}\':'.format(build_command=build_command))
+        subprocess.call(build_command,
+                        shell=True,
+                        cwd=demo_dir,
+                        stdout=sys.stdout,
+                        stderr=sys.stderr)
+# build_library
+
+# main
+print('reading \'{filename}\': '.format(filename=filename.split('/')[-1]), end='')
+config = ConfigParser.ConfigParser()
+try:
+    config.readfp(open(filename))
+except:
+    print('nothing to do (does not exist)')
+    pass
+demos = config.sections()
+if len(demos) == 0:
+    print('nothing to do (no demos specified)')
+    exit
+else:
+    for demo in demos:
+        print(demo + ' ', end='')
+    print('')
+
+for demo in demos:
+    print(demo + ':')
+    if not config.has_option(demo, 'build'):
+        raise Exception('missing \'build=\'list_of\', \'some_commands\'\' in section \'[{demo}]\''.format(demo=demo))
+    demo_is_built = build_demo(demo, config.get(demo, 'build'))
+    if demo_is_built:
+        if config.has_option(demo, 'msg'):
+            msg = config.get(demo, 'msg').lstrip().rstrip()
+            if not (msg[0] == '\'' and msg[-1] == '\''):
+                print('WARNING: \'msg\' should be of the form \'some example message\'!')
+            print('  {msg}'.format(msg=msg[1:-1]))
+        else:
+            print('  built successfuly')
+    else:
+        print('  built failed')
