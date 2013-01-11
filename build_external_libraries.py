b5b3cfa228f23d8c24bad30633eb6b6dc8680945
Felix Albrecht <felix.albrecht@uni-muenster.de>
2013-01-11 13:58:56 +0100

    [build_external_libraries] added abort flag

--- build_external_libraries.py
+++ build_external_libraries.py
@@ -12,10 +12,14 @@ import common
 filename = common.external_libraries_cfg_filename
 
 
-def build_library(library, build_commands):
+def build_library(library, config):
     src_dir = join(common.SRCDIR(), library)
     if not os.path.isdir(src_dir):
         raise Exception('\'{path}\' is not a directory (did you forget to run \'./local/bin/download_external_libraries.py\'?)!'.format(path=src_dir))
+    build_commands = config.get(library, 'build')
+    abort = True
+    if config.has_option(library, 'abort'):
+        abort = config.getboolean(library, 'abort')
     for build_command in build_commands.split(','):
         build_command = build_command.lstrip().rstrip()
         if not (build_command[0] == '\'' and build_command[-1] == '\''):
@@ -28,11 +32,14 @@ def build_library(library, build_commands):
         build_command = build_command.replace('$CXX', '{CXX}'.format(CXX=common.CXX()))
         build_command = build_command.replace('$F77', '{F77}'.format(F77=common.F77()))
         print('  calling \'{build_command}\':'.format(build_command=build_command))
-        subprocess.call(build_command,
-                        shell=True,
-                        cwd=src_dir,
-                        stdout=sys.stdout,
-                        stderr=sys.stderr)
+        ret = subprocess.call(build_command,
+                              shell=True,
+                              env=common.env(),
+                              cwd=src_dir,
+                              stdout=sys.stdout,
+                              stderr=sys.stderr)
+        if ret != 0 and abort:
+            break
 # build_library
 
 # main
@@ -55,4 +62,4 @@ for library in libraries:
     print(library + ':')
     if not config.has_option(library, 'build'):
         raise Exception('missing \'build=\'list_of\', \'some_commands\'\' in section \'[{library}]\''.format(library=library))
-    build_library(library, config.get(library, 'build'))
+    build_library(library, config)
