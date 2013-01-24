dde054efd33c8e1f6d5c706b6e7ee432143d1107
Felix Albrecht <mail@felixalbrecht.de>
2013-01-24 16:04:31 +0100

    [gen_path] added additional PKG_CONFIG_PATH

--- gen_path.py
+++ gen_path.py
@@ -7,9 +7,9 @@ import common as common
 
 with open(join(common.BASEDIR(), 'PATH.sh'), 'w') as pathfile:
     pathfile.write('export BASEDIR={BASEDIR}\n'.format(BASEDIR=common.BASEDIR()))
-    pathfile.write('export PATH=$BASEDIR/local/bin:$PATH\n')
-    pathfile.write('export LD_LIBRARY_PATH=$BASEDIR/local/lib:$LD_LIBRARY_PATH\n')
-    pathfile.write('export PKG_CONFIG_PATH=$BASEDIR/local/lib/pkgconfig:$PKG_CONFIG_PATH\n')
+    pathfile.write('export PATH=${BASEDIR}/local/bin:$PATH\n')
+    pathfile.write('export LD_LIBRARY_PATH=${BASEDIR}/local/lib:$LD_LIBRARY_PATH\n')
+    pathfile.write('export PKG_CONFIG_PATH=${BASEDIR}/local/lib/pkgconfig:${BASEDIR}/local/share/pkgconfig:$PKG_CONFIG_PATH\n')
     pathfile.write('export CC={CC}\n'.format(CC=common.CC()))
     pathfile.write('export CXX={CXX}\n'.format(CXX=common.CXX()))
     pathfile.write('export F77={F77}\n'.format(F77=common.F77()))
