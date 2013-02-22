a929a710ecba9119d0c2c21f982c26a2a8b12e9e
Felix Albrecht <mail@felixalbrecht.de>
2013-02-22 14:49:18 +0100

    [download_external_libraries] added less verbose output if called with any argument

--- download_external_libraries.py
+++ download_external_libraries.py
@@ -15,9 +15,14 @@ import common
 
 filename = common.external_libraries_cfg_filename
 
+verbose = True
+if len(sys.argv) > 1:
+    verbose = False
+
 
 def download_library(library, src):
-    print('  downloading from \'{src}\'... '.format(src=src), end='')
+    if verbose:
+        print('  downloading from \'{src}\'... '.format(src=src), end='')
     sys.stdout.flush()
     dest = join(common.SRCDIR(), src.split('/')[-1])
     filename = join(common.SRCDIR(), dest)
@@ -25,58 +30,88 @@ def download_library(library, src):
     if os.path.exists(filename):
         (filename, h) = urllib.urlretrieve(filename)
         filetype = h['Content-Type']
-        print('not necessary (already exists)')
+        if verbose:
+            print('not necessary (already exists)')
     else:
         (filename, headers) = urllib.urlretrieve(src, dest)
         filetype = headers['Content-Type']
-        print('done')
-    print('  unpacking \'{filename}\' '.format(filename=filename.split('/')[-1]), end='')
+        if verbose:
+            print('done')
+    if verbose:
+        print('  unpacking \'{filename}\' '.format(filename=filename.split('/')[-1]), end='')
     if filetype.startswith('application/x-gzip') or filetype.startswith('application/x-tar'):
         tar = tarfile.open(filename)
         # get the leading directory name
         names = tar.getnames()
         extracted_dir_name = names[0].split('/')[0]
-        print('to \'{extracted_dir_name}\'... '.format(extracted_dir_name=extracted_dir_name), end='')
+        if verbose:
+            print('to \'{extracted_dir_name}\'... '.format(extracted_dir_name=extracted_dir_name), end='')
         if not os.path.abspath(join(common.SRCDIR(), extracted_dir_name)).startswith(common.SRCDIR()):
-            raise Exception('unsafe filename in tar: \'{unsafe_name}\'!'.format(unsafe_name=extracted_dir_name))
+            if verbose:
+                print('unsafe filename in tar: \'{unsafe_name}\', aborting!'.format(unsafe_name=extracted_dir_name))
+            return False
         for name in names:
             if not (name.startswith(extracted_dir_name + '/') or name is extracted_dir_name):
-                raise Exception('tars containing more than one toplevel dir not supported!')
+                if verbose:
+                    print('tars containing more than one toplevel dir not supported, aborting!')
+                return False
         if os.path.exists(join(common.SRCDIR(), extracted_dir_name)):
-            print('not necessary (already exists)')
+            if verbose:
+                print('not necessary (already exists)')
         else:
             tar.extractall(common.SRCDIR())
-            print('done')
+            if verbose:
+                print('done')
         tar.close()
     else:
-        raise Exception('file is of wrong type (is \'{is_type}\')!'.format(is_type=filetype))
-    print('  moving \'{src}\' to \'{dest}\'... '.format(src=extracted_dir_name, dest=library), end='')
+        if verbose:
+            print('file is of wrong type (is \'{is_type}\'), aborting!'.format(is_type=filetype))
+        return False
+    if verbose:
+        print('  moving \'{src}\' to \'{dest}\'... '.format(src=extracted_dir_name, dest=library), end='')
     sys.stdout.flush()
     if os.path.exists(join(common.SRCDIR(), library)):
-        print('not necessary (already exists)')
+        if verbose:
+            print('not necessary (already exists)')
     else:
         shutil.move(join(common.SRCDIR(), extracted_dir_name), join(common.SRCDIR(), library))
-        print('done')
+        if verbose:
+            print('done')
+    return True
 # def download_library
 
 
 def git_clone_library(library, src):
-    print('  cloning \'{src}\':'.format(src=src))
+    if verbose:
+        print('  cloning \'{src}\':'.format(src=src))
     sys.stdout.flush()
     if os.path.exists(join(common.SRCDIR(), library)):
-        print('not necessary (already exists)')
+        if verbose:
+            print('not necessary (already exists)')
+        return True
     else:
-        ret = subprocess.call('git clone ' + src + ' ' + library,
-                      shell=True,
-                      env=common.env(),
-                      cwd=common.SRCDIR(),
-                      stdout=sys.stdout,
-                      stderr=sys.stderr)
+        if verbose:
+            ret = subprocess.call('git clone ' + src + ' ' + library,
+                                  shell=True,
+                                  env=common.env(),
+                                  cwd=common.SRCDIR(),
+                                  stdout=sys.stdout,
+                                  stderr=sys.stderr)
+        else:
+            with open(os.devnull, "w") as devnull:
+                ret = subprocess.call('git clone ' + src + ' ' + library,
+                                      shell=True,
+                                      env=common.env(),
+                                      cwd=common.SRCDIR(),
+                                      stdout=devnull,
+                                      stderr=devnull)
+        return not bool(ret)
 # git_clone_library
 
 
 # main
-print('reading \'{filename}\': '.format(filename=filename.split('/')[-1]), end='')
+if verbose:
+    print('reading \'{filename}\': '.format(filename=filename.split('/')[-1]), end='')
 config = ConfigParser.ConfigParser()
 try:
     config.readfp(open(filename))
@@ -84,18 +119,37 @@ except:
     raise Exception('Could not open \'{filename}\' with configparser!'.format(filename=filename))
 libraries = config.sections()
 if len(libraries) == 0:
-    print(' no external libraries specified')
+    if verbose:
+        print(' no external libraries specified')
     exit
 else:
     for library in libraries:
-        print(library + ' ', end='')
-    print('')
+        if verbose:
+            print(library + ' ', end='')
+    if verbose:
+        print('')
 
+failures = 0
 for library in libraries:
-    print(library + ':')
-    if config.has_option(library, 'git'):
-        git_clone_library(library, config.get(library, 'git'))
-    elif config.has_option(library, 'src'):
-        download_library(library, config.get(library, 'src'))
+    if verbose:
+        print(library + ':')
     else:
-        raise Exception('missing \'src=some_url\' or \'git=some_git_url\' in section \'{library]\''.format(library=library))
+        print('  ' + library + '... ', end='')
+        sys.stdout.flush()
+    success = False
+    if config.has_option(library, 'git'):
+        success = git_clone_library(library, config.get(library, 'git'))
+    elif config.has_option(library, 'src'):
+        success = download_library(library, config.get(library, 'src'))
+    else:
+        if verbose:
+            print('missing \'src=some_url\' or \'git=some_git_url\' in section \'{library]\', aborting!'.format(library=library))
+    if not verbose:
+        if success:
+            print('done')
+        else:
+            failures += 1
+            print('failed')
+if not verbose:
+    if failures > 0:
+        print('  call \'./local/bin/download_external_libraries.py\' manually to examine errors')
