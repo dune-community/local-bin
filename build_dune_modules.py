2ab4d5337c0cef408a6879a259984100bd16e5a1
Felix Albrecht <mail@felixalbrecht.de>
2013-02-22 14:50:03 +0100

    [build_dune_modules] added less verbose output if called with any argument

--- build_dune_modules.py
+++ build_dune_modules.py
@@ -4,22 +4,32 @@ from __future__ import print_function
 import ConfigParser
 import subprocess
 import sys
+import os
+
 
 import common
 
 filename = common.dune_modules_cfg_filename
 
+verbose = True
+if len(sys.argv) > 1:
+    verbose = False
+
+
 # read config opts
-print('reading \'{filename}\''.format(filename=filename.split('/')[-1]), end='')
+if verbose:
+    print('reading \'{filename}\''.format(filename=filename.split('/')[-1]), end='')
 config = ConfigParser.ConfigParser()
 try:
     config.readfp(open(filename))
 except:
-    print(': not possible', end='')
+    if verbose:
+        print(': not possible', end='')
     pass
 #    raise Exception('Could not open \'{filename}\' with configparser!'.format(filename=filename))
 # extract all commands
 commands = []
+short_commands = []
 if 'general' in config.sections():
     all_ = 'all'
     if config.has_option('general', 'all'):
@@ -40,6 +50,8 @@ if 'general' in config.sections():
                                 + ' {a_}').format(config_opts=common.config_opts_filename(),
                                                  dune_module=dune_module,
                                                  a_=a_))
+                short_commands.append(('{dune_module}:' + ' {a_}').format(dune_module=dune_module,
+                                                                          a_=a_))
     else:
         for a_ in all_.split():
             commands.append('./dune-common/bin/dunecontrol --opts={config_opts} {a_}'.format(config_opts=common.config_opts_filename(),
@@ -48,16 +60,33 @@ else:
     commands.append('./dune-common/bin/dunecontrol --opts={config_opts} all'.
                     format(config_opts=common.config_opts_filename()))
 # execute all commands
-print(', will be calling:')
-for command in commands:
-    print('  ' + command)
-for command in commands:
-    print('calling ' + command + ':')
-    ret = subprocess.call(command,
-                          shell=True,
-                          env=common.env(),
-                          cwd=common.BASEDIR(),
-                          stdout=sys.stdout,
-                          stderr=sys.stderr)
+if verbose:
+    print(', will be calling:')
+    for command in commands:
+        print('  ' + command)
+for command, short_command in zip(commands, short_commands):
+    ret = 1
+    if verbose:
+        print('calling ' + command + ':')
+        ret = subprocess.call(command,
+                              shell=True,
+                              env=common.env(),
+                              cwd=common.BASEDIR(),
+                              stdout=sys.stdout,
+                              stderr=sys.stderr)
+    else:
+        print('  ' + short_command + '... ', end='')
+        sys.stdout.flush()
+        with open(os.devnull, "w") as devnull:
+            ret = subprocess.call(command,
+                                  shell=True,
+                                  env=common.env(),
+                                  cwd=common.BASEDIR(),
+                                  stdout=devnull,
+                                  stderr=devnull)
+        if ret == 0:
+            print('done')
+        else:
+            print('failed')
     if ret != 0:
         sys.exit(1)
