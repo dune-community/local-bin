f54c2f7b3d0de73005b8937a55ce5b7f388f6989
Felix Albrecht <mail@felixalbrecht.de>
2013-02-22 14:49:43 +0100

    [build_external_libraries] added less verbose output if called with any argument

--- build_external_libraries.py
+++ build_external_libraries.py
@@ -11,6 +11,10 @@ import common
 
 filename = common.external_libraries_cfg_filename
 
+verbose = True
+if len(sys.argv) > 1:
+    verbose = False
+
 
 def build_library(library, config):
     src_dir = join(common.SRCDIR(), library)
@@ -24,7 +28,9 @@ def build_library(library, config):
     for build_command in build_commands.split(','):
         build_command = build_command.lstrip().rstrip()
         if not (build_command[0] == '\'' and build_command[-1] == '\''):
-            raise Exception('build commands have to be of the form \'command_1\' (is {cmd})!'.format(cmd=build_command))
+            if verbose:
+                ('build commands have to be of the form \'command_1\' (is {cmd}), aborting!'.format(cmd=build_command))
+                return False
         build_command = build_command[1:-1].lstrip().rstrip()
         build_command = build_command.replace('$BASEDIR', '{BASEDIR}'.format(BASEDIR=common.BASEDIR()))
         build_command = build_command.replace('$SRCDIR', '{SRCDIR}'.format(SRCDIR=common.SRCDIR()))
@@ -32,20 +38,30 @@ def build_library(library, config):
         build_command = build_command.replace('$CC', '{CC}'.format(CC=common.CC()))
         build_command = build_command.replace('$CXX', '{CXX}'.format(CXX=common.CXX()))
         build_command = build_command.replace('$F77', '{F77}'.format(F77=common.F77()))
-        print('  calling \'{build_command}\':'.format(build_command=build_command))
-        ret += subprocess.call(build_command,
-                              shell=True,
-                              env=common.env(),
-                              cwd=src_dir,
-                              stdout=sys.stdout,
-                              stderr=sys.stderr)
+        if verbose:
+            print('  calling \'{build_command}\':'.format(build_command=build_command))
+            ret += subprocess.call(build_command,
+                                  shell=True,
+                                  env=common.env(),
+                                  cwd=src_dir,
+                                  stdout=sys.stdout,
+                                  stderr=sys.stderr)
+        else:
+            with open(os.devnull, "w") as devnull:
+                ret += subprocess.call(build_command,
+                                      shell=True,
+                                      env=common.env(),
+                                      cwd=src_dir,
+                                      stdout=devnull,
+                                      stderr=devnull)
         if ret != 0:
             return not bool(ret)
     return not bool(ret)
 # build_library
 
 # main
-print('reading \'{filename}\': '.format(filename=filename.split('/')[-1]), end='')
+if verbose:
+    print('reading \'{filename}\': '.format(filename=filename.split('/')[-1]), end='')
 config = ConfigParser.ConfigParser()
 try:
     config.readfp(open(filename))
@@ -57,22 +73,42 @@ if len(libraries) == 0:
     exit
 else:
     for library in libraries:
-        print(library + ' ', end='')
-    print('')
+        if verbose:
+            print(library + ' ', end='')
+    if verbose:
+        print('')
 
 reports = []
+failure = 0
 for library in libraries:
+    if not verbose:
+        print('  ' + library + '... ', end='')
+        sys.stdout.flush()
+    success = False
     if not config.has_option(library, 'build'):
-        raise Exception('missing \'build=\'list_of\', \'some_commands\'\' in section \'[{library}]\''.format(library=library))
-    success = build_library(library, config)
-    if success:
-        reports.append(library + ': succeeded')
+        if verbose:
+            print('missing \'build=\'list_of\', \'some_commands\'\' in section \'[{library}]\', aborting!'.format(library=library))
     else:
-        reports.append(library + ': failed')
+        success = build_library(library, config)
+    if success:
+        if not verbose:
+            print('done')
+        else:
+            reports.append(library + ': succeeded')
+    else:
+        if not verbose:
+            failure += 1
+            print('failed')
+        else:
+            reports.append(library + ': failed')
 
-if len(reports) > 0:
-    print('================================================')
-    print('tried to build the following external libraries:')
-    print('================================================')
-    for report in reports:
-        print(report)
+if verbose:
+    if len(reports) > 0:
+        print('================================================')
+        print('tried to build the following external libraries:')
+        print('================================================')
+        for report in reports:
+            print(report)
+else:
+    if failure > 0:
+        print('  call \'./local/bin/build_external_libraries.py\' manually to examine errors')
