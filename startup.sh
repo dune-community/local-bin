be228b524bb08318c07e2729ddbe9ffc6350a2ee
Felix Albrecht <mail@felixalbrecht.de>
2013-06-23 21:58:22 +0200

    [startup] disabled logfiles and hopefully enabled break on failure

--- startup.sh
+++ startup.sh
@@ -1,56 +1,32 @@
 #!/bin/bash
 
-# create log dir
-LOG_DIR=$(mktemp -d /tmp/dune_demos.XXXXXXXX)
-LOG_FILE=$LOG_DIR/test.log
-touch $LOG_FILE &> /dev/null
-if [ $? != 0 ] ; then
-  echo "Error: could not create: $LOG_DIR/test.log." >&2
-  exit 1
-else
-  rm $LOG_FILE &> /dev/null
-fi
-
 echo -ne "writing path definitions... "
-LOG_FILE=$LOG_DIR/gen_PATH.log
-./local/bin/gen_path.py &> $LOG_FILE
+./local/bin/gen_path.py
 if [ $? == 0 ] ; then
-  source PATH &>> $LOG_FILE
-  echo "done (run '. PATH.sh' from now on)"
+  source PATH.sh
+  echo "done (run 'source PATH.sh' from now on)"
 else
-  echo "failed (see $LOG_FILE for details)" >&2
+  echo "failed" >&2
   exit 1
 fi
 
 echo "downloading external libraries:"
-LOG_FILE=$LOG_DIR/download_external_libraries.log
 ./local/bin/download_external_libraries.py 1
-#if [ $? == 0 ] ; then
-  #echo "done"
-#else
-  #echo "failed (see $LOG_FILE for details)" >&2
-  #exit 1
-#fi
+if [ $? != 0 ] ; then
+  exit 1
+fi
 
 echo "building external libraries:"
-LOG_FILE=$LOG_DIR/build_external_libraries.log
 ./local/bin/build_external_libraries.py 1
-#if [ $? == 0 ] ; then
-  #echo "done"
-#else
-  #echo "failed (see $LOG_FILE for details)" >&2
-  #exit 1
-#fi
+if [ $? != 0 ] ; then
+  exit 1
+fi
 
 echo "building dune modules:"
-LOG_FILE=$LOG_DIR/build_dune_modules.log
 ./local/bin/build_dune_modules.py 1
-#if [ $? == 0 ] ; then
-  #echo "done"
-#else
-  #echo "failed (see $LOG_FILE for details)" >&2
-  #exit 1
-#fi
+if [ $? != 0 ] ; then
+  exit 1
+fi
 
 echo "building demos:"
 ./local/bin/build_demos.py 1
