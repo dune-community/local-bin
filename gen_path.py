e6dceb512f6219527a0160326ff32961313455ba
Stephan Rave <stephanrave@uni-muenster.de>
2013-07-29 19:12:12 +0200

    [gen_path] updated virtualenv

--- gen_path.py
+++ gen_path.py
@@ -15,4 +15,4 @@ with open(join(common.BASEDIR(), 'PATH.sh'), 'w') as pathfile:
     pathfile.write('export F77={F77}\n'.format(F77=common.F77()))
     pathfile.write('export CXXFLAGS=\'{CXXFLAGS}\'\n'.format(CXXFLAGS=common.CXXFLAGS()))
     pathfile.write('export PYTHON_VERSION=2.7\n')
-    pathfile.write('[ -e $BASEDIR/local/bin/activate ] && source $BASEDIR/local/bin/activate\n')
+    pathfile.write('[ -e $BASEDIR/virtualenv/bin/activate ] && source $BASEDIR/virtualenv/bin/activate\n')
