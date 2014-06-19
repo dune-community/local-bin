8d698f3288a58530a5fdfdfb782e64619cef3d81
Rene Milk <rene.milk@uni-muenster.de>
2014-06-19 22:39:51 +0200

    started refactoring with build_external_libraries
    use logging instead of prints, remove code dups

--- build_external_libraries.py
+++ build_external_libraries.py
@@ -1,35 +1,29 @@
 #!/usr/bin/env python2.7
 
-from __future__ import print_function
+from __future__ import with_statement
+
 import ConfigParser
 import os
 from os.path import join
 import subprocess
 import sys
-
+import logging
 import common
 
 filename = common.external_libraries_cfg_filename
 
-verbose = True
-if len(sys.argv) > 1:
-    verbose = False
+verbose = len(sys.argv) <= 1
 
+log = logging.getLogger('external_libraries.build')
+log_lvl = logging.DEBUG if verbose else logging.INFO
+log.setLevel(log_lvl)
 
 def build_library(library, config):
-    no_dir = False
-    if config.has_option(library, 'only_build'):
-        no_dir = bool(config.get(library, 'only_build'))
-    if no_dir:
-        src_dir = common.SRCDIR()
-    else:
-        src_dir = join(common.SRCDIR(), library)
+    no_dir = config.getboolean(library, 'only_build')
+    src_dir = common.SRCDIR() if no_dir else join(common.SRCDIR(), library)
     if not os.path.isdir(src_dir):
         raise Exception('\'{path}\' is not a directory (did you forget to run \'./local/bin/download_external_libraries.py\'?)!'.format(path=src_dir))
     build_commands = config.get(library, 'build')
-#    abort = True
-#    if config.has_option(library, 'abort'):
-#        abort = config.getboolean(library, 'abort')
     ret = 0
     for build_command in build_commands.split(','):
         build_command = build_command.lstrip().rstrip()
@@ -44,78 +38,59 @@ def build_library(library, config):
         build_command = build_command.replace('$CC', '{CC}'.format(CC=common.CC()))
         build_command = build_command.replace('$CXX', '{CXX}'.format(CXX=common.CXX()))
         build_command = build_command.replace('$F77', '{F77}'.format(F77=common.F77()))
-        if verbose:
-            print('  calling \'{build_command}\':'.format(build_command=build_command))
+        log.debug('  calling \'{build_command}\':'.format(build_command=build_command))
+        with open(os.devnull, "w") as devnull:
+            err = sys.stderr if verbose else devnull
+            out = sys.stdout if verbose else devnull
             ret += subprocess.call(build_command,
                                   shell=True,
                                   env=common.env(),
                                   cwd=src_dir,
-                                  stdout=sys.stdout,
-                                  stderr=sys.stderr)
-        else:
-            with open(os.devnull, "w") as devnull:
-                ret += subprocess.call(build_command,
-                                      shell=True,
-                                      env=common.env(),
-                                      cwd=src_dir,
-                                      stdout=devnull,
-                                      stderr=devnull)
+                                  stdout=out,
+                                  stderr=err)
         if ret != 0:
             return not bool(ret)
     return not bool(ret)
 # build_library
 
 # main
-if verbose:
-    print('reading \'{filename}\': '.format(filename=filename.split('/')[-1]), end='')
-config = ConfigParser.ConfigParser()
-try:
-    config.readfp(open(filename))
-except:
+log.debug('reading \'{filename}\': '.format(filename=filename.split('/')[-1]))
+config = ConfigParser.SafeConfigParser({'only_build': 'False'})
+if not os.path.isfile(filename):
     raise Exception('Could not open \'{filename}\' with configparser!'.format(filename=filename))
+config.read(filename)
 libraries = config.sections()
 if len(libraries) == 0:
-    print(' no external libraries specified')
-    exit
+    raise Exception(' no external libraries specified')
 else:
     for library in libraries:
-        if verbose:
-            print(library + ' ', end='')
-    if verbose:
-        print('')
+        log.debug(library + ' ')
+    log.debug('')
 
 reports = []
 failure = 0
 for library in libraries:
-    if not verbose:
-        print('  ' + library + '... ', end='')
-        sys.stdout.flush()
-    success = False
+    log.info('  ' + library + '... ')
     if not config.has_option(library, 'build'):
-        if verbose:
-            print('missing \'build=\'list_of\', \'some_commands\'\' in section \'[{library}]\', aborting!'.format(library=library))
-    else:
-        success = build_library(library, config)
+        raise Exception('missing \'build=\'list_of\', \'some_commands\'\' in section \'[{library}]\', aborting!'.format(library=library))
+    success = build_library(library, config)
     if success:
-        if not verbose:
-            print('done')
-        else:
-            reports.append(library + ': succeeded')
+        log.info('done')
+        reports.append(library + ': succeeded')
     else:
-        if not verbose:
-            failure += 1
-            print('failed')
-        else:
-            reports.append(library + ': failed')
+        failure += 1
+        log.info('failed')
+        reports.append(library + ': failed')
 
 if verbose:
     if len(reports) > 0:
-        print('================================================')
-        print('tried to build the following external libraries:')
-        print('================================================')
+        log.debug('''
+================================================
+tried to build the following external libraries:
+================================================''')
         for report in reports:
-            print(report)
+            log.debug(report)
 else:
     if failure > 0:
-        print('  call \'./local/bin/build_external_libraries.py\' manually to examine errors')
+        log.critical('  call \'./local/bin/build_external_libraries.py\' manually to examine errors')
 sys.exit(failure)
