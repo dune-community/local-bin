41018fceebf9e88a736b076da7a21a70447d5063
Felix Schindler <felix.schindler@wwu.de>
2014-08-15 14:15:14 +0200

    [gen_path] fix the fix

--- gen_path.py
+++ gen_path.py
@@ -10,11 +10,11 @@ if __name__ == '__main__':
     with open(join(local_config.basedir, 'PATH.sh'), 'w') as pathfile:
         pathfile.write('export BASEDIR={}\n'.format(local_config.basedir))
         pathfile.write('export PATH=${BASEDIR}/local/bin:$PATH\n')
-        pathfile.write('export LD_LIBRARY_PATH=${basedir}/local/lib64:${basedir}/local/lib:$LD_LIBRARY_PATH\n')
+        pathfile.write('export LD_LIBRARY_PATH=${BASEDIR}/local/lib64:${BASEDIR}/local/lib:$LD_LIBRARY_PATH\n')
         pathfile.write(
-            'export PKG_CONFIG_PATH=${basedir}/local/lib64/pkgconfig:${basedir}/local/lib/pkgconfig:${basedir}/local/share/pkgconfig:$PKG_CONFIG_PATH\n')
+            'export PKG_CONFIG_PATH=${BASEDIR}/local/lib64/pkgconfig:${BASEDIR}/local/lib/pkgconfig:${BASEDIR}/local/share/pkgconfig:$PKG_CONFIG_PATH\n')
         pathfile.write('export CC={CC}\n'.format(CC=local_config.cc))
         pathfile.write('export CXX={CXX}\n'.format(CXX=local_config.cxx))
         pathfile.write('export F77={F77}\n'.format(F77=local_config.f77))
         pathfile.write('export PYTHON_VERSION=2.7\n')
-        pathfile.write('[ -e $basedir/virtualenv/bin/activate ] && . $basedir/virtualenv/bin/activate\n')
+        pathfile.write('[ -e $BASEDIR/virtualenv/bin/activate ] && . $BASEDIR/virtualenv/bin/activate\n')
