5d46e46cb3047af0fef5a60d05616e10f4d92a10
Rene Milk <rene.milk@uni-muenster.de>
2016-03-29 15:09:31 +0200

    uses Exceptions instead of return codes

--- download_external_libraries.py
+++ download_external_libraries.py
@@ -19,12 +19,12 @@ import common
 
 log = common.get_logger('external_libraries.download')
 
+class DownloadException(Exception): pass
 
-def download_library(library, src):
-    config = common.LocalConfig(allow_for_broken_config_opts=True)
+def download_library(local_config, library, src):
     log.debug('  downloading from \'{src}\'... '.format(src=src), end='')
-    dest = join(config.srcdir, os.path.basename(src))
-    filename = join(config.srcdir, dest)
+    dest = join(local_config.srcdir, os.path.basename(src))
+    filename = join(local_config.srcdir, dest)
     if os.path.exists(filename):
         filename, h = urllib.urlretrieve(filename)
         filetype = h['Content-Type']
@@ -41,59 +41,55 @@ def download_library(library, src):
         names = tar.getnames()
         extracted_dir_name = names[0].split('/')[0]
         log.debug('to \'{extracted_dir_name}\'... '.format(extracted_dir_name=extracted_dir_name), end='')
-        if not os.path.abspath(join(config.srcdir, extracted_dir_name)).startswith(config.srcdir):
-            log.debug('unsafe filename in tar: \'{unsafe_name}\', aborting!'.format(unsafe_name=extracted_dir_name))
-            return False
+        if not os.path.abspath(join(local_config.srcdir, extracted_dir_name)).startswith(local_config.srcdir):
+            raise DownloadException('unsafe filename in tar: \'{unsafe_name}\', aborting!'.format(unsafe_name=extracted_dir_name))
         for name in names:
             if not (name.startswith(extracted_dir_name + '/') or name is extracted_dir_name):
-                log.debug('tars containing more than one toplevel dir not supported, aborting!')
-                return False
-        if os.path.exists(join(config.srcdir, extracted_dir_name)):
+                raise DownloadException('tars containing more than one toplevel dir not supported, aborting!')
+
+        if os.path.exists(join(local_config.srcdir, extracted_dir_name)):
             log.debug('not necessary (already exists)')
         else:
-            tar.extractall(config.srcdir)
+            tar.extractall(local_config.srcdir)
             log.debug('done')
         tar.close()
     else:
-        log.debug('file is of wrong type (is \'{is_type}\'), aborting!'.format(is_type=filetype))
-        return False
+        raise DownloadException('file is of wrong type (is \'{is_type}\'), aborting!'.format(is_type=filetype))
+
     log.debug('  moving \'{src}\' to \'{dest}\'... '.format(src=extracted_dir_name, dest=library), end='')
-    if os.path.exists(join(config.srcdir, library)):
+    if os.path.exists(join(local_config.srcdir, library)):
         log.debug('not necessary (already exists)')
     else:
-        shutil.move(join(config.srcdir, extracted_dir_name), join(config.srcdir, library))
+        shutil.move(join(local_config.srcdir, extracted_dir_name), join(local_config.srcdir, library))
         log.debug('done')
-    return True
 
-def git_clone_library(library, src):
-    local_config = common.LocalConfig(allow_for_broken_config_opts=True)
+
+def git_clone_library(local_config, library, src):
     log.debug('  cloning \'{src}\':'.format(src=src))
     if os.path.exists(join(local_config.srcdir, library)):
         log.debug('not necessary (already exists)')
-        return True
     else:
-        with open(os.devnull, "w") as devnull:
-            ret = subprocess.call('git clone ' + src + ' ' + library,
-                                  shell=True,
-                                  env=local_config.make_env(),
-                                  cwd=local_config.srcdir,
-                                  stdout=sys.stdout if common.VERBOSE else devnull,
-                                  stderr=sys.stderr if common.VERBOSE else devnull)
-        return not bool(ret)
+        with open(os.devnull, 'wb') as devnull:
+            subprocess.check_call('git clone ' + src + ' ' + library,
+                              shell=True,
+                              env=local_config.make_env(),
+                              cwd=local_config.srcdir,
+                              stdout=sys.stdout if common.VERBOSE else devnull,
+                              stderr=sys.stderr if common.VERBOSE else devnull)
 
 
-def download_all():
-    local_config = common.LocalConfig(allow_for_broken_config_opts=True)
+def download_all(local_config=None):
+    local_config = local_config or common.LocalConfig(allow_for_broken_config_opts=True)
     log.debug(
             'reading \'{filename}\': '.format(filename=os.path.basename(local_config.external_libraries_cfg_filename)),
             end='')
-    config = configparser.ConfigParser()
+    ext_libs_config = configparser.ConfigParser()
     try:
-        config.readfp(open(local_config.external_libraries_cfg_filename))
+        ext_libs_config.readfp(open(local_config.external_libraries_cfg_filename, mode='rb'))
     except:
         raise Exception('Could not open \'{filename}\' with configparser!'.format(
             filename=local_config.external_libraries_cfg_filename))
-    libraries = config.sections()
+    libraries = ext_libs_config.sections()
     if len(libraries) == 0:
         log.debug(' no external libraries specified')
         sys.exit(0)
@@ -104,33 +100,31 @@ def download_all():
     for library in libraries:
         log.info('  ' + library + '... ', end='')
         log.debug('')
-        success = False
-        download = True
-        if config.has_option(library, 'only_build'):
-            download = not bool(config.get(library, 'only_build'))
-        if download:
-            if config.has_option(library, 'git'):
-                success = git_clone_library(library, config.get(library, 'git'))
-            elif config.has_option(library, 'src'):
-                success = download_library(library, config.get(library, 'src'))
+
+        if ext_libs_config.has_option(library, 'only_build') and ext_libs_config.getboolean(library, 'only_build'):
+            log.info('nothing to do, since \'only_build\' is True')
+            continue
+        try:
+            if ext_libs_config.has_option(library, 'git'):
+                git_clone_library(local_config=local_config, library=library, src=ext_libs_config.get(library, 'git'))
+            elif ext_libs_config.has_option(library, 'src'):
+                download_library(local_config=local_config, library=library, src=ext_libs_config.get(library, 'src'))
             else:
                 log.debug('missing \'src=some_url\' or \'git=some_git_url\' in section \'{library}\', aborting!'.format(
                         library=library))
-            if not common.VERBOSE:
-                if success:
-                    log.info('done')
-                else:
-                    failures += 1
-                    log.info('failed')
-        else:
-            log.info('nothing to do, since \'only_build\' is True')
+            log.info('done')
+        except Exception as e:
+            failures += 1
+            log.info('failed')
+            log.debug(e)
+
     if failures > 0:
-        log.error('  call \'./local/bin/download_external_libraries.py\' manually to examine errors')
-    sys.exit(failures)
+        log.critical('  call \'./local/bin/download_external_libraries.py\' manually to examine errors')
+    return failures
 
 
 
-TESTDATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'testdata')
+TESTDATA_DIR = common.TESTDATA_DIR
 
 @pytest.fixture(params=[os.path.join(root, fn)
                         for root,_, files in os.walk(os.path.join(TESTDATA_DIR, 'ext_configs')) for fn in files])
@@ -138,10 +132,10 @@ def config_filename(request):
     return request.param
 
 def test_shipped_configs(config_filename):
-    os.environ['OPTS'] = os.path.join(TESTDATA_DIR, '..', 'config.opts', 'clang')
+    os.environ['OPTS'] = os.path.join(TESTDATA_DIR, 'config.opts', 'clang')
     os.environ['INSTALL_PREFIX'] = '/tmp'
-    download_all()
+    download_all(local_config=common.mk_config(external_libraries=config_filename))
 
 
 if __name__ == '__main__':
-    download_all()
+    sys.exit(download_all())
