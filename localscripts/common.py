2e821d6d797314c2e7810266ab318de09f0a20b5
Rene Milk <rene.milk@wwu.de>
2016-04-21 16:52:49 +0200

    rewire config opts parsing to more closely dunecontrol
    we now run the files thru a bash shell making it
    possible to use stacks of them via 'source'

--- localscripts/common.py
+++ localscripts/common.py
@@ -39,6 +39,7 @@ class LocalConfig(object):
         self.config_opts_filename = ''
         self.boost_toolsets = {'gcc-{}.{}'.format(i, j): 'gcc' for i, j in itertools.product(range(4, 7), range(10))}
         self.boost_toolsets.update({'icc': 'intel-linux', 'clang': 'clang'})
+        self.config_opts = {}
         if allow_for_broken_config_opts:
             try:
                 self._parse_config_opts()
@@ -57,23 +58,35 @@ class LocalConfig(object):
             cc = os.environ.get(string)
             if cc is not None:
                 return cc
-            for possible in self.config_opts:
-                possible = list(shlex.shlex(possible))
-                if len(possible) > 1 and possible[0] == string and possible[1] == '=':
-                    return ''.join(possible[2:])
-            return default
+            self.config_opts.get(string, default)
 
         self.cc = find_opt('CC', default='gcc')
         self.cxx = find_opt('CXX', default='g++')
         self.f77 = find_opt('F77', default='gfortran')
+
+    def _read_opts_file(self, filename):
+        # makes sure to throw IOError if file is missing
+        _ = open(filename).read()
+        env = os.environ.copy()
+        # prints only shell variables after sourcing the opts file
+        shell_vars = subprocess.check_output('set -o posix ; source {} ; set '.format(filename),
+                                             env=env, universal_newlines=True, shell=True, executable='/bin/bash')
+        parsed_vars = {}
+        for line in shlex.split(shell_vars):
+            try:
+                sep_idx = line.index('=')
+                parsed_vars[line[:sep_idx]] = shlex.split(line[sep_idx+1:])
+            except ValueError:
+                pass #means we hit IFS or similar in parsed_vars
+        return parsed_vars
 
     def _try_opts(self, env):
         if 'OPTS' in env:
             possibles = (join(self.basedir, env['OPTS']), join(self.basedir, 'config.opts', env['OPTS']))
             for filename in possibles:
                 try:
-                    return filename, open(filename, mode='rt').read()
-                except IOError:
+                    return filename, self._read_opts_file(filename)
+                except IOError as e:
                     continue
             raise IOError('Environment defined OPTS not discovered in {}'.format(possibles))
         if 'CC' not in env:
@@ -84,35 +97,34 @@ class LocalConfig(object):
         for filename in (join(dirname, pref + cc) for dirname, pref in
                          itertools.product(search_dirs, prefixes)):
             try:
-                return filename, open(filename, mode='rt').read()
+                return filename, self._read_opts_file(filename)
             except IOError:
                 continue
         raise IOError('No suitable opts file for CC {} in anyof {} x {}'.format(cc, search_dirs, prefixes))
 
     def _get_config_opts(self, env):
         try:
-            self.config_opts_filename, config_opts = env['OPTS'], open(env['OPTS'], mode='rt').read()
+            self.config_opts_filename, config_opts = env['OPTS'], self._read_opts_file(env['OPTS'])
         except (IOError, KeyError):
             self.config_opts_filename, config_opts = self._try_opts(env)
 
-        def _extract_from(i, parts, flag_arg):
-            configure_flags = parts[i + 2]
-            configure_flags = [f.strip() for f in shlex.split(configure_flags, posix=True) if f != '\n']
+        def set_cxx_flags_from(configure_flags, flag_arg):
             for arg in configure_flags:
                 if arg.startswith(flag_arg):
-                    self.cxx_flags = ' '.join(shlex.split(arg[len(flag_arg):].strip().strip('='), posix=True))
-                    return configure_flags
+                    cxx_flags = shlex.split(arg[len(flag_arg)+1:].strip().strip('=').strip('\\').strip(), posix=True)
+                    cxx_flags = [ii for ii in cxx_flags if ii != '\n']
+                    self.cxx_flags = ' '.join(cxx_flags)
+                    return
+            raise RuntimeError(configure_flags)
 
-        parts = list(shlex.shlex(config_opts, posix=True))
-        for i, token in enumerate(parts):
-            if token == 'CONFIGURE_FLAGS':
-                flag_arg = 'CXXFLAGS'
-                return _extract_from(i, parts, flag_arg)
-            if token == 'CMAKE_FLAGS':
-                flag_arg = '-DCMAKE_CXX_FLAGS'
-                return _extract_from(i, parts, flag_arg)
-        raise RuntimeError('found neither CMAKE_FLAGS nor CONFIGURE_FLAGS in opts file {}'.format(
-                self.config_opts_filename))
+        if 'CONFIGURE_FLAGS' in config_opts:
+            set_cxx_flags_from(config_opts['CONFIGURE_FLAGS'], flag_arg='CXXFLAGS')
+        elif 'CMAKE_FLAGS' in config_opts:
+            set_cxx_flags_from(config_opts['CMAKE_FLAGS'], flag_arg='-DCMAKE_CXX_FLAGS')
+        else:
+            raise RuntimeError('found neither CMAKE_FLAGS nor CONFIGURE_FLAGS in opts file {}\n{}'.format(
+                self.config_opts_filename, config_opts))
+        return config_opts
 
     def make_env(self):
         env = os.environ.copy()
@@ -236,11 +248,12 @@ class BraceMessage(object):
 
 
 TESTDATA_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'testdata'))
+CONFIG_DIR = os.path.join(TESTDATA_DIR, 'config.opts')
 
-
-@pytest.fixture(params=[os.path.join(root, fn)
-                        for root,_, files in os.walk(os.path.join(TESTDATA_DIR, 'config.opts')) for fn in files])
+@pytest.fixture(params=[os.path.join(CONFIG_DIR, file)
+                        for file in os.listdir(CONFIG_DIR) if os.path.isfile(os.path.join(CONFIG_DIR, file)) ])
 def config_filename(request):
+    print('Checking {}'.format(request.param))
     return request.param
 
 
