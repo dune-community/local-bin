177356cf850b8e1645b0e71fb9f011dc084ff603
Rene Milk <rene.milk@wwu.de>
2016-04-26 15:54:41 +0200

    rewire path file generation + add test

--- localscripts/gen_path.py
+++ localscripts/gen_path.py
@@ -1,30 +1,68 @@
 #!/usr/bin/env python2.7
 
 from __future__ import print_function, absolute_import, with_statement
+
+import string
+import os
 from os.path import join
 
 from localscripts import common
 
+pathfile_tpl ='''
+export BASEDIR=${BASEDIR}
+export INSTALL_PREFIX=${INSTALL_PREFIX}
+export PATH=${INSTALL_PREFIX}/bin:$PATH
+export LD_LIBRARY_PATH=${INSTALL_PREFIX}/lib64:${INSTALL_PREFIX}/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=${INSTALL_PREFIX}/lib64/pkgconfig:${INSTALL_PREFIX}/lib/pkgconfig:${INSTALL_PREFIX}/share/pkgconfig:$PKG_CONFIG_PATH
+export CC=${CC}
+export CXX=${CXX}
+export F77=${F77}
+export PYTHON_VERSION=2.7
+[ -e ${INSTALL_PREFIX}/local/bin/activate ] && . ${INSTALL_PREFIX}/local/bin/activate
+export OMP_NUM_THREADS=1
+export SIMDB_PATH=${INSTALL_PREFIX}/DATA
+export QUEUE_DIRECTORY=${INSTALL_PREFIX}/QUEUE
+'''
 
-def gen_path():
-    local_config = common.LocalConfig()
+
+def _fill_tpl(local_config):
+    tpl = string.Template(pathfile_tpl)
+    return tpl.safe_substitute(INSTALL_PREFIX=local_config.install_prefix,
+                               BASEDIR=local_config.basedir,
+                               CC=local_config.cc, CXX=local_config.cxx,
+                               F77=local_config.f77)
+
+
+def gen_path(config=None):
+    local_config = config or common.LocalConfig()
     with open(join(local_config.basedir, 'PATH.sh'), 'wb') as pathfile:
         # see common._prep_build_command on how to make this more elegant
-        pathfile.write('export BASEDIR={}\n'.format(local_config.basedir))
-        pathfile.write('export INSTALL_PREFIX={}\n'.format(local_config.install_prefix))
-        pathfile.write('export PATH=${INSTALL_PREFIX}/bin:$PATH\n')
-        pathfile.write('export LD_LIBRARY_PATH=${INSTALL_PREFIX}/lib64:${INSTALL_PREFIX}/lib:$LD_LIBRARY_PATH\n')
-        pathfile.write(
-            'export PKG_CONFIG_PATH=${INSTALL_PREFIX}/lib64/pkgconfig:${INSTALL_PREFIX}/lib/pkgconfig:${INSTALL_PREFIX}/share/pkgconfig:$PKG_CONFIG_PATH\n')
-        pathfile.write('export CC={CC}\n'.format(CC=local_config.cc))
-        pathfile.write('export CXX={CXX}\n'.format(CXX=local_config.cxx))
-        pathfile.write('export F77={F77}\n'.format(F77=local_config.f77))
-        pathfile.write('export PYTHON_VERSION=2.7\n')
-        pathfile.write('[ -e ${INSTALL_PREFIX}/local/bin/activate ] && . ${INSTALL_PREFIX}/local/bin/activate\n')
-        pathfile.write('export OMP_NUM_THREADS=1\n')
-        pathfile.write('export SIMDB_PATH=${INSTALL_PREFIX}/DATA\n')
-        pathfile.write('export QUEUE_DIRECTORY=${INSTALL_PREFIX}/QUEUE\n')
+        pathfile.write(_fill_tpl(local_config))
 
 
-if __name__ == '__main__':
-    gen_path()
+class dummy_config(object):
+    cc = 'compiler'
+    cxx = 'cxx_compiler'
+    f77 = 'fortran_compiler'
+    install_prefix = '/home'
+    basedir = '/tmp'
+
+
+def test_template():
+    config = dummy_config()
+    expected = '''
+export BASEDIR=/tmp
+export INSTALL_PREFIX=/home
+export PATH=/home/bin:$PATH
+export LD_LIBRARY_PATH=/home/lib64:/home/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/home/lib64/pkgconfig:/home/lib/pkgconfig:/home/share/pkgconfig:$PKG_CONFIG_PATH
+export CC=compiler
+export CXX=cxx_compiler
+export F77=fortran_compiler
+export PYTHON_VERSION=2.7
+[ -e /home/local/bin/activate ] && . /home/local/bin/activate
+export OMP_NUM_THREADS=1
+export SIMDB_PATH=/home/DATA
+export QUEUE_DIRECTORY=/home/QUEUE
+'''
+    assert expected == _fill_tpl(config)
