276aa6f619dcaca4f500e90bd45ceaaf16cc4f24
Felix Schindler <felix.schindler@wwu.de>
2017-07-26 17:47:13 +0200

    [gen_path] do not mix install prefix with basedir substitution

--- localscripts/gen_path.py
+++ localscripts/gen_path.py
@@ -10,13 +10,13 @@ from localscripts import common
 pathfile_tpl ='''
 export BASEDIR=${BASEDIR}
 export INSTALL_PREFIX=${INSTALL_PREFIX}
-export PATH=${INSTALL_PREFIX}/bin:$PATH
-export LD_LIBRARY_PATH=${INSTALL_PREFIX}/lib64:${INSTALL_PREFIX}/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=${INSTALL_PREFIX}/lib64/pkgconfig:${INSTALL_PREFIX}/lib/pkgconfig:${INSTALL_PREFIX}/share/pkgconfig:$PKG_CONFIG_PATH
+export PATH=${_INST_PREF_}/bin:$PATH
+export LD_LIBRARY_PATH=${_INST_PREF_}/lib64:${_INST_PREF_}/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=${_INST_PREF_}/lib64/pkgconfig:${_INST_PREF_}/lib/pkgconfig:${_INST_PREF_}/share/pkgconfig:$PKG_CONFIG_PATH
 export CC=${CC}
 export CXX=${CXX}
 export F77=${F77}
-[ -e ${INSTALL_PREFIX}/bin/activate ] && . ${INSTALL_PREFIX}/bin/activate
+[ -e ${_INST_PREF_}/bin/activate ] && . ${_INST_PREF_}/bin/activate
 export OMP_NUM_THREADS=1
 '''
 
@@ -26,7 +26,7 @@ def _fill_tpl(local_config):
     return tpl.safe_substitute(INSTALL_PREFIX=local_config.install_prefix,
                                BASEDIR=local_config.basedir,
                                CC=local_config.cc, CXX=local_config.cxx,
-                               F77=local_config.f77)
+                               F77=local_config.f77, _INST_PREF_="${INSTALL_PREFIX}")
 
 
 def gen_path(config=None):
