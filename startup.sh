342cb943d32c64f691c58729b048ce6aa585303c
René Milk <rene.milk@wwu.de>
2017-11-29 12:05:46 +0100

    make the startup script useful again

--- startup.sh
+++ startup.sh
@@ -1,33 +1,23 @@
 #!/bin/bash
-BASEDIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; cd .. ;  pwd -P )"
 
+set -e
+
+export OPTS="${1}"
+
+BASEDIR="$(cd "$(dirname ${BASH_SOURCE[0]})" ; cd .. ;  pwd -P )"
 cd ${BASEDIR}
 
 echo -ne "writing path definitions... "
 ./bin/gen_path.py
-if [ $? == 0 ] ; then
-  source PATH.sh
-  echo "done (run 'source PATH.sh' from now on)"
-else
-  echo "failed" >&2
-  exit 1
-fi
+source PATH.sh
+echo "done (run 'source PATH.sh' from now on)"
 
 echo "downloading external libraries:"
 ./bin/download_external_libraries.py
-if [ $? != 0 ] ; then
-  exit 1
-fi
 
 echo "building external libraries:"
 ./bin/build_external_libraries.py
-if [ $? != 0 ] ; then
-  exit 1
-fi
 
 echo "building dune modules:"
-dune_setup ${1}
-if [ $? != 0 ] ; then
-  exit 1
-fi
+dune_setup ${OPTS}
 
