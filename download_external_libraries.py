5f56f65363443c281b510141f50bf5e2ee0fcc37
Tobias Leibner <tobias.leibner@googlemail.com>
2020-02-26 13:10:09 +0100

    explicitly use python3

--- download_external_libraries.py
+++ download_external_libraries.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import localscripts.download_external_libraries as dl
--- gen_path.py
+++ gen_path.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from localscripts.gen_path import gen_path
 from localscripts.common import make_config
